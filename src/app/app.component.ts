1f4f659439fa6d0c9c6da489bf079b0f86dbfc02
mohit-sojitra <mohit.sojitra@bacancy.com>
2021-07-27 12:12:18 +0530

    tab with cookies added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tab-layout';
+  activeTab: string;
+
+  constructor() {
+    this.activeTab = this.getCookie('activeTab') ? this.getCookie('activeTab') : 'tab1';
+  }
+
+  tabClickHandler(activeTab: string) {
+    this.activeTab = activeTab;
+    this.setCookie('activeTab', this.activeTab, 3);
+  }
+
+
+  private setCookie(
+    name: string,
+    value: string,
+    expireDays: number,
+  ) {
+    let d: Date = new Date();
+    d.setTime(d.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    let expires: string = `expires=${d.toUTCString()}`;
+    document.cookie = `${name}=${value}; ${expires}`;
+  }
+
+  private getCookie(name: string) {
+    let ca:string[]= document.cookie.split(';');
+    let value =  ca.find(value => value.trim().split('=')[0] == name);
+    return value ? value.split('=')[1] : '';
+  }
 }
